69d83be3c595f44c26b1669442405c337a538423
Olliv3r <oliveobom100@gmail.com>
2018-12-08 13:14:27 -0400

    $

--- shell.sh
+++ shell.sh
@@ -13,10 +13,10 @@ clear
 menuOs() {
 	setterm --foreground blue
 	figlet Shell
-	echo "	\033[01;94m'---------------'"
-	echo " 	\033[01;94m' \033[01;92m{\033[0m1\033[01;92m} \033[01;91mTermux OS \033[01;94m'"
-	echo "	\033[01;94m' \033[01;92m{\033[0m2\033[01;92m} \033[01;91mOutros OS \033[01;94m'"
-	echo "  \033[01;94m	'---------------'"
+	echo "	$blue'-------------------'"
+	echo " 	$blue' $green{$white 1 $green} $red Termux OS $blue '"
+	echo "	$blue' $green{$white 2 $green} $red Outros OS $blue '"
+	echo "  $blue	'-------------------'"
 }
 
 menuOs
@@ -35,29 +35,28 @@ case $resp in
 		echo "$cyan Script que personaliza seu termux do seu jeito, tudo de forma automatica $white"
 		echo ''
 
-		echo "{01} Parrot"
-		echo "{02} Kali 1"
-		echo "{03} Kali 2"
-		echo "{04} Ubuntu  $cyan > $green Version: $red v2.5 $white"
-		echo "{05} Debian"
-		echo "{06} Fedora"
-		echo "{07} CentOs"
-		echo "{08} OpenSuse 1"
-		echo "{09} OpenSuse 2"
-		#echo "$white[$blue<11$blue>$white]$red Kali Oficial"
-echo ""
+		echo "$green{$white 01 $green}$red Parrot"
+		echo "$green{$white 02 $green}$red Kali 1"
+		echo "$green{$white 03 $green}$red Kali 2"
+		echo "$green{$white 04 $green}$red Ubuntu  $cyan > $green Version: $red v2.6 $white"
+		echo "$green{$white 05 $green}$red Debian"
+		echo "$green{$white 06 $green}$red Fedora"
+		echo "$green{$white 07 $green}$red CentOs"
+		echo "$green{$white 08 $green}$red OpenSuse 1"
+		echo "$green{$white 09 $green}$red OpenSuse 2"
+		echo ""
 
 		function menu_return() {
-		echo $green"Returning..."
-		bash $HOME/Shell/shell.sh
+			echo $green"Returning..."
+			bash $HOME/Shell/shell.sh
 		}
 
 		menu() {
-			echo -n $blue"Ps1> "$yellow
+			echo -n $blue"Shell> "$yellow
 			read resp
 		}
 
-		echo -n $blue"A-shell> "$red
+		echo -n $blue"shell> "$red
 		read resp
 
 		case $resp in
@@ -302,21 +301,21 @@ echo ""
 		figlet Shell
                 echo "$cyan Script que personaliza seu termux do seu jeito, tudo de forma automatica $white"
 		echo ''
-		echo "{01} Parrot"
-		echo "{02} Kali 1"
-		echo "{03} Kali 2"
-                echo "{04} Ubuntu  $cyan > $green Version: $red v2.5 $white"
-                echo "{05} Debian"
-                echo "{06} Fedora"
-                echo "{07} CentOs"
-                echo "{08} OpenSuse 1"
-		echo "{09} OpenSuse 2"
-		echo "{10} Kali Oficial"
-echo ""
+		echo "$green{$white 01 $green}$red Parrot"
+		echo "$green{$white 02 $green}$red Kali 1"
+		echo "$green{$white 03 $green}$red Kali 2"
+                echo "$green{$white 04 $green}$red Ubuntu  $cyan > $green Version: $red v2.6 $white"
+                echo "$green{$white 05 $green}$red Debian"
+                echo "$green{$white 06 $green}$red Fedora"
+                echo "$green{$white 07 $green}$red CentOs"
+                echo "$green{$white 08 $green}$red OpenSuse 1"
+		echo "$green{$white 09 $green}$red OpenSuse 2"
+		echo "$green{$white 10 $green}$red Kali Oficial"
+		echo ""
 
                 function menu_return() {
-                echo $green"Returning..."
-                bash /root/Shell/hell/shell.sh
+			echo $green"Returning..."
+			bash /root/Shell/hell/shell.sh
                 }
 
                 menu() {
@@ -555,7 +554,7 @@ echo ""
                                 sed -i "s/NomeMachine/$NomeDaMaquina/g" /home/.bashrc
                                 echo $green"Configurando..."
                                 sleep 2
-                                echo $yellow"Abra uma nova aba do termux para visualizar sua nova configuracao da variavel de ambiente PS1 "
+     	                           echo $yellow"Abra uma nova aba do termux para visualizar sua nova configuracao da variavel de ambiente PS1 "
                                 menu
                                 case $resp in
                                         "return" | "r") echo ''
